a30ee3ad6e24a47bcbea7d6f80e33e54a3be8b73
Patcharaphorn Lertvirul <kukkik.kpl@gmail.com>
2020-02-16 00:29:53 +0700

    add contact section

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ $(document).ready(function () {
     });
 });
 
-var nystories = document.getElementById('education').offsetTop;
+var nystories = document.getElementById('skill').offsetTop;
 window.onscroll = function () {
     if (window.pageYOffset > 0) {
         var opac = (window.pageYOffset / nystories);
-        document.getElementById('background').style.backgroundImage = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url('images/profile3.gif')";
+        document.getElementById('background').style.backgroundImage = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url('images/profile-resize.png')";
     }
 }
 
