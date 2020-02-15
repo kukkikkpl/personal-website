d16aa2489b035450ef23e9f8598a25850762377f
Patcharaphorn Lertvirul <kukkik.kpl@gmail.com>
2020-02-15 17:09:47 +0700

    split script from HTML file

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('#navbar-toggler').click(function () {
+        $('#nav-icon').toggleClass('open');
+    });
+});
+
+var nystories = document.getElementById('education').offsetTop;
+window.onscroll = function () {
+    if (window.pageYOffset > 0) {
+        var opac = (window.pageYOffset / nystories);
+        document.getElementById('background').style.backgroundImage = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url('images/profile3.png')";
+    }
+}
+
+$('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+    $('#nav-icon').toggleClass('open');
+});
