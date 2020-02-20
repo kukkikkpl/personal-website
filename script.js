5fe8b21e218df699d8d4879a195b63558a0c961a
Patcharaphorn Lertvirul <kukkik.kpl@gmail.com>
2020-02-21 01:04:24 +0700

    add scrolling effect

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ $(document).ready(function () {
     $('#navbar-toggler').click(function () {
         $('#nav-icon').toggleClass('open');
     });
-});
+    var topPosition = document.getElementById('bio').offsetTop;
+    var divs = $('.hide');
+    window.onscroll = function () {
+        if (window.pageYOffset > 0) {
+            var opac = (window.pageYOffset / topPosition);
+            document.getElementById('background').style.backgroundImage = "linear-gradient(rgba(255, 255, 255, " + opac + ") 0%, rgba(255, 255, 255, " + opac + ") 60%), url('images/profile-resize.png')";
+        }
+        $.each(divs, function (i, item) {
+            if ($(item).offset().top <= $(window).scrollTop() + $(window).height()) {
+                $(item).animate({opacity: 1}, 1000);
+            }
 
-var nystories = document.getElementById('skill').offsetTop;
-window.onscroll = function () {
-    if (window.pageYOffset > 0) {
-        var opac = (window.pageYOffset / nystories);
-        document.getElementById('background').style.backgroundImage = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url('images/profile-resize.png')";
+        });
     }
-}
+    $('.navbar-nav>li>a').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+        $('#nav-icon').removeClass('open');
+    });
 
-$('.navbar-nav>li>a').on('click', function () {
-    $('.navbar-collapse').collapse('hide');
-    $('#nav-icon').toggleClass('open');
 });
+
+
+
